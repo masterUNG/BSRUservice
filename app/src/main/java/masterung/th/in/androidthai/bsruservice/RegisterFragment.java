3360fdf439afb38294bbf300e426b9d0287f6103
masterUNG <phrombutr@gmail.com>
2019-03-04 10:19:45 +0700

    Toolbar Success

--- app/src/main/java/masterung/th/in/androidthai/bsruservice/RegisterFragment.java
+++ app/src/main/java/masterung/th/in/androidthai/bsruservice/RegisterFragment.java
@@ -2,7 +2,9 @@ package masterung.th.in.androidthai.bsruservice;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +20,28 @@ public class RegisterFragment extends Fragment {
         // Required empty public constructor
     }
 
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle(getString(R.string.register));
+        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle("Please Fill All Blank");
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+
+
+    }   // Main Method
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
